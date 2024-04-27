0628cb517f3cd7a15a110f4d5c60d831dd30676a
Derrick <oseiagyemanb@gmail.com>
2024-04-27 15:37:18 +0000

    update of file with Product and Stock component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter } from 'react-router-dom';
 import Hero from './components/Hero';
+import Product from './components/Product';
+import Stock from './components/Stock';
 
 const App = () => {
 	return (
 		<BrowserRouter>
 			<Navbar />
 			<Hero />
+			<Product />
+			<Stock />
 		</BrowserRouter>
 	);
 };
