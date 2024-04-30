67ff75d83bfdd096a12a15c58734d990d46d7846
Derrick <oseiagyemanb@gmail.com>
2024-04-30 10:13:57 +0000

    initial commit

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FaArrowTrendUp } from 'react-icons/fa6';
+import HighlightCard from './HighlightCard';
+import { highlight_items } from '../data';
+
+const Highlights = () => {
+	return (
+		<section className='bg-cyan-600 mt-0'>
+			<div className='align-element py-20 grid items-center gap-8'>
+				<section className='flex flex-col md:mx-20 items-center'>
+					<div className='flex items-center justify-center capitalize py-3 px-6 '>
+						<span>
+							<FaArrowTrendUp className='text-cyan-950' />
+						</span>
+						<h6 className='ml-2 uppercase font-semibold text-cyan-50'>
+							key highlights
+						</h6>
+					</div>
+					<p className='text-4xl tracking-wide text-cyan-50 md:text-4xl md:w-45 md:mx-25 text-center font-semibold'>
+						Reasons for choosing us
+					</p>
+				</section>
+				<div className='mt-5 grid sm:grid-cols-2 xl:grid-cols-3 gap-8 xl:divide-y xl:divide-x'>
+					{highlight_items.map((item) => {
+						return <HighlightCard key={item.id} {...item} />;
+					})}
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Highlights;
