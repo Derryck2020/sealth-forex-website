8e53d328640b384c32e289dec723af0a105941e3
Derrick <oseiagyemanb@gmail.com>
2024-05-01 10:44:07 +0000

    update of files

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero';
 import Product from './components/Product';
 import Stock from './components/Stock';
 import Highlights from './components/Highlights';
+import Testimonials from './components/Testimonials';
 
 const App = () => {
 	return (
@@ -14,6 +15,7 @@ const App = () => {
 			<Product />
 			<Stock />
 			<Highlights />
+			<Testimonials />
 		</BrowserRouter>
 	);
 };
--- src/data.jsx
+++ src/data.jsx
@@ -11,6 +11,16 @@ import {
 	microsoft_img,
 	eurousd_img,
 	amazon_img,
+	expert,
+	support,
+	profit,
+	no_hidden,
+	technology,
+	transparency,
+	maria,
+	elaine,
+	ben,
+	nathan,
 } from './assets';
 
 export const navlinks = [
@@ -97,5 +107,83 @@ export const stockItems = [
 		icon: microsoft_img,
 		trend: <FaArrowTrendUp className='text-green-600' />,
 		price: 406.32,
+	},
+];
+
+export const highlight_items = [
+	{
+		id: nanoid(),
+		icon: expert,
+		title: 'friendly & expert',
+		text: 'Our company provides reliable guidance for successful forex investment.',
+	},
+	{
+		id: nanoid(),
+		icon: support,
+		title: '24/7 support',
+		text: 'Assistance to clients is always availiable, ensuring prompt support whenever they need it.',
+	},
+	{
+		id: nanoid(),
+		icon: profit,
+		title: 'best profit split',
+		text: 'Fair and equitable distribution of profits for all stakeholders.',
+	},
+	{
+		id: nanoid(),
+		icon: no_hidden,
+		title: 'no hidden rules',
+		text: 'Transparent policies with no concealed regulations or undisclosed conditions.',
+	},
+	{
+		id: nanoid(),
+		icon: technology,
+		title: 'Advanced Technology',
+		text: 'State-of-the-art trading platforms with real-time market data.',
+	},
+	{
+		id: nanoid(),
+		icon: transparency,
+		title: 'Transparency',
+		text: 'We are committed to providing accurate and timely information, fair pricing.',
+	},
+];
+
+export const testimonial_list = [
+	{
+		id: nanoid(),
+		name: 'maria font',
+		image: maria,
+		location: 'london, uk',
+		title: 'impressive results',
+		text: 'Have been trading with this forex company for over a year now, and I am consistently impressed with the results. Their expert analysis and guidance have helped me achieve steady profits.',
+		rating: 5,
+	},
+	{
+		id: nanoid(),
+		name: 'ben staham',
+		image: ben,
+		location: 'Texas, USA',
+		title: 'Exceptional Support',
+		text: 'They are always available to assist me with any questions or concerns I have, providing prompt and helpful responses',
+		rating: 4,
+	},
+	{
+		id: nanoid(),
+		name: 'elaine waters',
+		image: elaine,
+		location: 'dortmund, German',
+		title: 'Transparent and Trustworthy',
+		text: 'They operate with integrity and honesty, ensuring that there are no hidden fees or surprises. I feel confident trading with them.',
+		rating: 5,
+	},
+	{
+		id: nanoid(),
+		name: 'nathan jeffery',
+		image: nathan,
+		location: 'liverpool, UK',
+		title: 'Advanced Technology, Easy to Use',
+		text: 'The trading platform provided by this forex company is top-notch. It offers advanced technology with user-friendly features, making it easy for me to execute trades and monitor the market.',
+		rating: 5,
 	},
 ];
