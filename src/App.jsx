06aaec665c9fe40b5b40468ae200cd810c8db8e0
Derrick <oseiagyemanb@gmail.com>
2024-05-03 22:09:31 +0000

    update of file

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Hero from './components/Hero';
 import Product from './components/Product';
 import Stock from './components/Stock';
 import Highlights from './components/Highlights';
 import Testimonials from './components/Testimonials';
 import Footer from './components/Footer';
+import { About, Service, Blog, Contact } from './pages/index.jsx';
 
 const App = () => {
 	return (
-		<BrowserRouter>
+		<Router>
 			<Navbar />
+			<Routes>
+				<Route exact path='/' component={Hero} />
+				<Route path='/about' component={<About />} />
+				<Route path='/services' component={<Service />} />
+				<Route path='/blog' component={<Blog />} />
+				<Route path='/contact' component={<Contact />} />
+			</Routes>
 			<Hero />
 			<Product />
 			<Stock />
 			<Highlights />
 			<Testimonials />
 			<Footer />
-		</BrowserRouter>
+		</Router>
 	);
 };
 
