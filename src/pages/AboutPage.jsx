140974cd657de9aae510a6a3f59518f33602d506
Derrick <oseiagyemanb@gmail.com>
2024-05-03 22:08:45 +0000

    initial commit of files

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const About = () => {
+	return <section className='bg-cyan-950 mt-0'>About Page</section>;
+};
+
+export default About;
---
+++ src/pages/BlogPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageHero from '../components/PageHero';
+
+const Blog = () => {
+	return (
+		<main>
+			<PageHero title='blog' />
+			<section className='bg-cyan-500'>Blog Section</section>
+		</main>
+	);
+};
+
+export default Blog;
