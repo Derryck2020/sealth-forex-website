a117c8ead34d2dd9f5a51414b02c2a4f84f5c3c9
Derrick <oseiagyemanb@gmail.com>
2024-05-07 23:09:45 +0000

    initial commit of file

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { default as About } from './AboutPage';
+export { default as Blog } from './BlogPage';
+export { default as Contact } from './ContactPage';
+export { default as Service } from './ServicePage';
+export { default as Home } from './HomePage';
+export { default as Landing } from './LandingPage';
+export { default as ErrorPage } from './ErrorPage';
