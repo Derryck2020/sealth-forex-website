a4555b9752125b961525397e1e031e99d3ba5bda
Derrick <oseiagyemanb@gmail.com>
2024-05-07 11:19:08 +0000

    update of file with correct syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Hero from './components/Hero';
-import Product from './components/Product';
-import Stock from './components/Stock';
-import Highlights from './components/Highlights';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
-import { About, Service, Blog, Contact } from './pages/index.jsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home, About, Service, Blog, Contact } from './pages';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Home />,
+	},
+	{
+		path: 'about',
+		element: <About />,
+	},
+	{
+		path: 'services',
+		element: <Service />,
+	},
+	{
+		path: 'blog',
+		element: <Blog />,
+	},
+	{
+		path: 'contact',
+		element: <Contact />,
+	},
+]);
 
 const App = () => {
-	return (
-		<Router>
-			<Navbar />
-			<Routes>
-				<Route exact path='/' component={Hero} />
-				<Route path='/about' component={<About />} />
-				<Route path='/services' component={<Service />} />
-				<Route path='/blog' component={<Blog />} />
-				<Route path='/contact' component={<Contact />} />
-			</Routes>
-			<Hero />
-			<Product />
-			<Stock />
-			<Highlights />
-			<Testimonials />
-			<Footer />
-		</Router>
-	);
+	return <RouterProvider router={router} />;
 };
 
 export default App;
