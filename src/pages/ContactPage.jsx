bd7bb3d605cc30a296cc9e3548dfd4883de1bd79
Derrick <oseiagyemanb@gmail.com>
2024-05-07 23:04:30 +0000

    update of file with correct syntax

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
 
 const Contact = () => {
-	return <div>ContactPage</div>;
+	return (
+		<>
+			<section>
+				<h2>Contact Page Working</h2>
+			</section>
+		</>
+	);
 };
 
 export default Contact;
