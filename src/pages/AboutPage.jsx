f6db89d2af2248349c8072151e7eaaf81f90c18d
Derrick <oseiagyemanb@gmail.com>
2024-05-09 11:39:40 +0000

    update of file

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
+import { croppedImg } from '../assets';
+import { FaArrowTrendUp } from 'react-icons/fa6';
 
 const AboutPage = () => {
 	return (
 		<>
-			<section>
-				<h2>About Page Working</h2>
+			<header>
+				<div
+					style={{
+						backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.9), rgba(44,174,186,0.7)), url(${croppedImg})`,
+					}}
+					className='bg-cover bg-center h-[400px]'
+				>
+					<h2 className='align-element flex justify-center text-center capitalize text-cyan-50 md:w-[750px] text-4xl pt-32 font-semibold tracking-wide'>
+						Get to know the heart and soul behind our Forex trading firm.
+					</h2>
+				</div>
+			</header>
+			<section className='flex flex-col mt-10 md:mx-20 items-center'>
+				<div className='flex items-center py-3 px-6 gap-2'>
+					<span>
+						<FaArrowTrendUp className='text-cyan-500' />
+					</span>
+					<h2 className='text-xl font-bold text-cyan-950'>
+						{' '}
+						<span>About </span>
+						seal<span className='text-cyan-500 text-2xl'>TH</span>
+					</h2>
+				</div>
+				<h2 className='text-4xl capitalize tracking-wider text-cyan-950 md:text-6xl md:w-45 md:mx-25 text-center font-semibold'>
+					Pioneers in proprietary forex trading
+				</h2>
 			</section>
 		</>
 	);
