f9dbbc2dbb610bc4da38913843922cb38478a102
Derrick <oseiagyemanb@gmail.com>
2024-05-11 23:36:01 +0000

    update of file

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -7,7 +7,7 @@ const Highlights = () => {
 	return (
 		<section className='bg-cyan-600 mt-0'>
 			<div className='align-element py-20 grid items-center gap-8'>
-				<section className='flex flex-col md:mx-20 items-center'>
+				<div className='flex flex-col md:mx-20 items-center'>
 					<div className='flex items-center justify-center capitalize py-3 px-6 '>
 						<span>
 							<FaArrowTrendUp className='text-cyan-950' />
@@ -19,7 +19,7 @@ const Highlights = () => {
 					<p className='text-4xl capitalize tracking-wide text-cyan-50 md:text-4xl md:w-45 md:mx-25 text-center font-semibold'>
 						Reasons for choosing us
 					</p>
-				</section>
+				</div>
 				<div className='mt-5 grid sm:grid-cols-2 xl:grid-cols-3 gap-8'>
 					{highlight_items.map((item) => (
 						<div key={item.id}>
