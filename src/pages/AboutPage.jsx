c0b94a8a7108dca635da1e86c9e1dc21be24a9c6
Derrick <oseiagyemanb@gmail.com>
2024-05-14 09:50:12 +0000

    update of file

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { croppedImg } from '../assets';
 import { FaArrowTrendUp } from 'react-icons/fa6';
-import { about_details } from '../data';
+import { about_details, professional_details } from '../data';
 
 const AboutPage = () => {
 	return (
@@ -42,16 +42,66 @@ const AboutPage = () => {
 									{item.icon}
 								</span>
 							</div>
-							<div className='flex flex-col text-cyan-950 gap-2'>
-								<h2 className='text-xl capitalize font-semibold'>
+							<div className='flex flex-col gap-2'>
+								<h2 className='text-xl text-cyan-950 capitalize font-semibold'>
 									{item.topic}
 								</h2>
-								<p className=''>{item.text}</p>
+								<p className='text-cyan-900'>{item.text}</p>
 							</div>
 						</article>
 					))}
 				</div>
 			</section>
+			<div className='bg-cyan-50 mx-5 xl:mx-40 my-10 border border-cyan-200 rounded-md'>
+				<section className='align-element flex flex-col my-5 md:mx-10 gap-14'>
+					<div className='flex flex-col items-center'>
+						<div className='flex items-center py-3 px-6 gap-2'>
+							<span>
+								<FaArrowTrendUp className='text-cyan-500' />
+							</span>
+							<h2 className='text-xl uppercase font-bold text-cyan-950'>
+								trust the professionals
+							</h2>
+						</div>
+						<p className=' tracking-wider text-cyan-800 md:text-md md:mx-25 text-center'>
+							We are a group of passionate, independent thinkers who
+							never stop exploring new ways to improve trading for the
+							investor.
+						</p>
+					</div>
+					<div className='grid gap-10 md:gap-y-20 lg:grid-cols-2'>
+						{professional_details.map((item) => (
+							<article key={item.id} className='flex flex-cols gap-4'>
+								<img
+									src={item.icon}
+									alt={item.name}
+									className='text-white grid object-cover size-40 justify-center items-center rounded-xl'
+								/>
+
+								<div className='flex flex-col gap-2 justify-between'>
+									<h4 className='text-sm text-cyan-800 uppercase'>
+										{item.position}
+									</h4>
+									<h2 className='text-lg text-cyan-900 capitalize font-semibold'>
+										{item.name}
+									</h2>
+									<p className='text-cyan-800 text-sm'>{item.text}</p>
+									<ul className='flex flex-row gap-5'>
+										{item.links.map((link) => (
+											<li
+												key={link.id}
+												className='text-cyan-800 hover:text-cyan-500 transition-style'
+											>
+												<a href={link.href}>{link.icon}</a>
+											</li>
+										))}
+									</ul>
+								</div>
+							</article>
+						))}
+					</div>
+				</section>
+			</div>
 		</>
 	);
 };
