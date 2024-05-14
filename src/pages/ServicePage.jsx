8a46d43693e0534ebe3f1069e79ceeb3d8ca4685
Derrick <oseiagyemanb@gmail.com>
2024-05-14 09:50:29 +0000

    update of file

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
+import { croppedImg } from '../assets';
+import Product from '../components/Product';
 
 const ServicePage = () => {
 	return (
 		<>
 			<section>
-				<h2>Service Page under Working</h2>
+				<div
+					style={{
+						backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.9), rgba(44,174,186,0.7)), url(${croppedImg})`,
+					}}
+					className='bg-cover bg-center h-[400px]'
+				>
+					<h2 className='align-element flex justify-center text-center capitalize text-cyan-50 md:w-[750px] text-4xl pt-32 font-semibold tracking-wide'>
+						Our services
+					</h2>
+				</div>
+			</section>
+			<section>
+				<Product />
 			</section>
 		</>
 	);
