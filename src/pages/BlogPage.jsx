de8c792eab8e7af385ba1223572789349adcebf8
Derrick <oseiagyemanb@gmail.com>
2024-05-16 19:23:36 +0000

    update of file

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
-import Navbar from '../components/Navbar';
-import Hero from '../components/Hero';
-import Footer from '../components/Footer';
+import { croppedImg } from '../assets';
+import { news_items } from '../data';
+import NewsCard from '../components/NewsCard';
 
 const BlogPage = () => {
 	return (
 		<>
 			<section>
-				<h2>Blog Page Working</h2>
+				<div
+					style={{
+						backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.9), rgba(44,174,186,0.7)), url(${croppedImg})`,
+					}}
+					className='bg-cover bg-center h-[400px]'
+				>
+					<h2 className='align-element flex justify-center text-center capitalize text-cyan-50 md:w-[750px] text-4xl pt-32 font-semibold tracking-wide'>
+						latest news and updates.
+					</h2>
+				</div>
+			</section>
+			<section className='align-element my-5 grid sm:grid-cols-2 gap-8'>
+				{news_items.map((product) => {
+					return <NewsCard key={product.id} {...product} />;
+				})}
 			</section>
 		</>
 	);
