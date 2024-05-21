048cc1004d44a390db2ea4f7f5bde2d4f188d229
Derrick <oseiagyemanb@gmail.com>
2024-05-21 21:49:55 +0000

    update of file

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { croppedImg, team } from '../assets';
 import { FaArrowTrendUp } from 'react-icons/fa6';
 import { about_details, professional_details } from '../data';
+import PageHero from '../components/PageHero';
 
 const AboutPage = () => {
 	return (
@@ -11,11 +12,12 @@ const AboutPage = () => {
 					style={{
 						backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.9), rgba(44,174,186,0.7)), url(${croppedImg})`,
 					}}
-					className='bg-cover bg-center h-[400px]'
+					className='grid bg-cover bg-center h-[400px]'
 				>
 					<h2 className='align-element flex justify-center text-center capitalize text-cyan-50 md:w-[750px] text-4xl pt-32 font-semibold tracking-wide'>
 						Get to know the heart and soul behind our Forex trading firm.
 					</h2>
+					<PageHero title='About Us' />
 				</div>
 			</section>
 			<section className='align-element flex flex-col mt-10 gap-14'>
