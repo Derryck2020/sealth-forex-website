97b4520abe495f4ed1e68f742fafd4b053eef26b
Derrick <oseiagyemanb@gmail.com>
2024-05-24 10:30:16 +0000

    update of file

--- src/pages/ServicePage.jsx
+++ src/pages/ServicePage.jsx
@@ -3,6 +3,7 @@ import { croppedImg, cloud } from '../assets';
 import { FaArrowTrendUp } from 'react-icons/fa6';
 import { products } from '../data';
 import ProductsCard from '../components/ProductsCard';
+import PageHero from '../components/PageHero';
 
 const ServicePage = () => {
 	return (
@@ -12,11 +13,12 @@ const ServicePage = () => {
 					style={{
 						backgroundImage: `linear-gradient(to bottom right, rgba(0,0,0,0.9), rgba(44,174,186,0.7)), url(${croppedImg})`,
 					}}
-					className='bg-cover bg-center h-[400px]'
+					className='grid bg-cover bg-center h-[400px]'
 				>
 					<h2 className='align-element flex justify-center text-center capitalize text-cyan-50 md:w-[750px] text-4xl pt-32 font-semibold tracking-wide'>
 						Our services
 					</h2>
+					<PageHero title='Services' />
 				</div>
 			</section>
 			<section>
