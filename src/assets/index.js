950c5464f3e5f4288017870c9a4b4d7fab647657
Derrick <oseiagyemanb@gmail.com>
2024-05-30 20:01:24 +0000

    update of file

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,73 @@
 import menu from '../assets/menu.svg';
 import close from '../assets/close.svg';
+import link_icon from '../assets/link.svg';
 import heroImg from '../assets/hero1.jpg';
+import research from '../assets/research.jpg';
+import currency from '../assets/currency-pair.jpg';
+import trading from '../assets/pamm-trading.jpg';
+import tesla_img from '../assets/tesla.jpeg';
+import gbpusd_img from '../assets/gbpusd.png';
+import microsoft_img from '../assets/microsoft.png';
+import eurousd_img from '../assets/eur-usd.png';
+import amazon_img from '../assets/amazon.svg';
+import expert from '../assets/interest.png';
+import support from '../assets/24-7.png';
+import profit from '../assets/profit.png';
+import transparency from '../assets/trans.png';
+import no_hidden from '../assets/no-hidden-fees.png';
+import technology from '../assets/tech.png';
+import ben from '../assets/ben.jpg';
+import elaine from '../assets/elaine.jpg';
+import maria from '../assets/maria.jpg';
+import nathan from '../assets/nathan.jpg';
+import croppedImg from '../assets/cropped.jpg';
+import ivan from '../assets/ivan.jpg';
+import freda from '../assets/freda.jpg';
+import bruce from '../assets/bruce.jpg';
+import brooke from '../assets/brooke.jpg';
+import team from '../assets/team-back.jpg';
+import cloud from '../assets/cloud.jpg';
+import gold from '../assets/gold.jpg';
+import meeting from '../assets/meeting.jpg';
+import money from '../assets/money.jpg';
+import sp_500 from '../assets/s&p-500.jpg';
+import apple from '../assets/apple.jpg';
+import hyundai from '../assets/hyundai.jpg';
 
-export { menu, close, heroImg };
+export {
+	gold,
+	meeting,
+	money,
+	sp_500,
+	apple,
+	hyundai,
+	menu,
+	close,
+	link_icon,
+	tesla_img,
+	gbpusd_img,
+	microsoft_img,
+	eurousd_img,
+	amazon_img,
+	heroImg,
+	research,
+	currency,
+	trading,
+	expert,
+	support,
+	profit,
+	transparency,
+	no_hidden,
+	technology,
+	maria,
+	elaine,
+	nathan,
+	ben,
+	croppedImg,
+	ivan,
+	bruce,
+	freda,
+	brooke,
+	team,
+	cloud,
+};
